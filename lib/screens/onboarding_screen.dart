cb59ff8b7d385b7a72691a7c74457b9db41c10f1
hanssantosoo <150638666+hanssantosoo@users.noreply.github.com>
2023-11-28 18:50:57 +0700

    Update onboarding_screen.dart

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,1 +1,53 @@
+import 'package:concentric_transition/concentric_transition.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:emoney/screens/login_screen.dart';
 
+class PageData {
+  final String? title;
+
+  final Image? mediaContent;
+  final Color bgColor;
+  final Color textColor;
+  final Widget? optionalWidget;
+
+  PageData(
+      {this.title,
+      this.mediaContent,
+      this.bgColor = const Color.fromRGBO(40, 4, 148, 0),
+      this.textColor = Colors.black,
+      this.optionalWidget});
+}
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  late List<PageData> pages;
+  late Widget getStarted;
+
+  @override
+  void initState() {
+    super.initState();
+
+    getStarted = Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(8.0),
+          child: TextButton(
+            onPressed: completedOrientation,
+            child: Text(
+              'Get Started',
+              style: TextStyle(fontSize: 27, color: Colors.white),
+            ),
+            style: TextButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 171, 69, 254),
+                padding: EdgeInsets.all(18),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.5))),
+          ),
+        ),
