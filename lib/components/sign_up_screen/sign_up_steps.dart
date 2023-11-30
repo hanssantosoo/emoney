be564025fae7a6cd85d87b5876efc60a83c8ca9a
hanssantosoo <150638666+hanssantosoo@users.noreply.github.com>
2023-12-01 01:29:21 +0700

    Update sign_up_steps.dart

--- lib/components/sign_up_screen/sign_up_steps.dart
+++ lib/components/sign_up_screen/sign_up_steps.dart
@@ -64,3 +64,139 @@ class _SignUpStepsState extends State<SignUpSteps> {
     _signUpStepController.dispose();
     super.dispose();
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.transparent,
+        width: double.infinity,
+        child: Column(
+          children: [
+            Container(
+              color: Colors.transparent,
+              width: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [0, 1, 2]
+                    .map((e) => Wrap(
+                          crossAxisAlignment: WrapCrossAlignment.center,
+                          children: [
+                            GestureDetector(
+                              onTap: () => changeStepOnTap(e),
+                              child: CircleAvatar(
+                                  backgroundColor: stepHasError[e]
+                                      ? Colors.red.shade600
+                                      : !stepCompletedSuccessfully[e]
+                                          ? Color(0xffF5F7FA)
+                                          : Colors.green.shade600,
+                                  foregroundColor: !stepCompletedSuccessfully[e]
+                                      ? Color(0xFF0070BA)
+                                      : Color.fromRGBO(40, 4, 148, 1),
+                                  radius: 18,
+                                  child: stepHasError[e]
+                                      ? Icon(
+                                          FluentIcons.warning_16_filled,
+                                          color: Colors.white,
+                                        )
+                                      : stepCompletedSuccessfully[e]
+                                          ? Icon(
+                                              FluentIcons.checkmark_16_regular)
+                                          : _currentStep == e
+                                              ? Icon(FluentIcons.edit_16_filled)
+                                              : Text("${e + 1}")),
+                            ),
+                            if (e < 2)
+                              Container(
+                                height: 10,
+                                width: 70,
+                                color: stepCompletedSuccessfully[e]
+                                    ? Colors.green.shade600
+                                    : Colors.transparent,
+                              ),
+                          ],
+                        ))
+                    .toList(),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Container(
+              width: double.infinity,
+              height: _currentStep == 2 ? 230 : 300,
+              child: PageView(
+                clipBehavior: Clip.none,
+                controller: _signUpStepController,
+                physics: NeverScrollableScrollPhysics(),
+                children: signUpStepContent,
+              ),
+            ),
+            if (_currentStep == 2)
+              Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 3.6, horizontal: 10),
+                  child: RichText(
+                      text: TextSpan(
+                          text: ' ',
+                          style:
+                              TextStyle(fontSize: 14, color: Color(0xFF929BAB)),
+                          children: <InlineSpan>[]))),
+            confirmSignUpButton
+                ? Container(
+                    margin: const EdgeInsets.symmetric(vertical: 16.0),
+                    width: double.infinity,
+                    height: 64,
+                    decoration: BoxDecoration(
+                      gradient: RadialGradient(
+                          colors: [Color(0xff0070BA), Color(0xff1546A0)],
+                          radius: 8.4,
+                          center: Alignment(-0.24, -0.36)),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: ElevatedButton(
+                        onPressed: _finalStepProccessing,
+                        child: Text(
+                          'Create Account',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w600),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.transparent,
+                          shadowColor: Colors.transparent,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                        )),
+                  )
+                : Row(
+                    children: [
+                      if (_currentStep > 0 && confirmSignUpButton == false)
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.transparent,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: TextButton(
+                              onPressed: _goBackToPreviousStep,
+                              child: Wrap(
+                                  crossAxisAlignment: WrapCrossAlignment.center,
+                                  spacing: 3.2,
+                                  children: [
+                                    Icon(
+                                      FluentIcons.arrow_left_16_filled,
+                                      color: Colors.blue,
+                                      size: 18,
+                                    ),
+                                    Text(
+                                      'Back',
+                                      style: TextStyle(
+                                          color: Colors.blue, fontSize: 16),
+                                    ),
+                                  ]),
+                              style: TextButton.styleFrom(
+                                primary: Colors.transparent,
+                                shadowColor: Colors.transparent,
+                                elevation: 0,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20)),
+                              )),
+                        ),
