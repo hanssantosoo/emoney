d4a007ee50b54bff6b2adb59f640d06e1d5e4330
Wansansss <113201428+Wansansss@users.noreply.github.com>
2023-11-30 14:47:58 +0700

    Update all_transaction_activities_screen.dart

--- lib/screens/all_transaction_activities_screen.dart
+++ lib/screens/all_transaction_activities_screen.dart
@@ -13,3 +13,87 @@ class AllTransactionActivities extends StatefulWidget {
   AllTransactionActivitiesState createState() =>
       AllTransactionActivitiesState();
 }
+
+class AllTransactionActivitiesState extends State<AllTransactionActivities> {
+  final List allTransactions = [
+    {
+      "transactionId": "1",
+      "transactionMemberName": "Hans Santoso",
+      "transactionMemberPhoneNumber": "09000000000",
+      "transactionMemberEmail": "hans1@gmail.com",
+      "moneySent": "+1000",
+      "date": "2023-11-01"
+    },
+    {
+      "transactionId": "2",
+      "transactionMemberName": "Jerel Jacob",
+      "transactionMemberPhoneNumber": "08000000000",
+      "transactionMemberEmail": "jerjb@gmail.com",
+      "moneySent": "-1000",
+      "date": "2023-11-01"
+    },
+    {
+      "transactionId": "3",
+      "transactionMemberName": "Johnny Jade",
+      "transactionMemberPhoneNumber": "07000000000",
+      "transactionMemberEmail": "jadej@gmail.com",
+      "moneySent": "+500",
+      "date": "2023-11-01"
+    },
+    {
+      "transactionId": "4",
+      "transactionMemberName": "Ivan Makmur",
+      "transactionMemberPhoneNumber": "06000000000",
+      "transactionMemberEmail": "ivan1@gmail.com",
+      "moneySent": "-81000",
+      "date": "2023-11-01"
+    },
+    {
+      "transactionId": "5",
+      "transactionMemberName": "Sabrina Phalosa",
+      "transactionMemberPhoneNumber": "05000000000",
+      "transactionMemberEmail": "sabsa@gmail.com",
+      "moneySent": "+1000",
+      "date": "2023-11-01"
+    },
+    {
+      "transactionId": "6",
+      "transactionMemberName": "Sabrina Phalosa",
+      "transactionMemberPhoneNumber": "05000000000",
+      "transactionMemberEmail": "sabsa@gmail.com",
+      "moneySent": "+1000",
+      "date": "2023-11-01"
+    },
+    {
+      "transactionId": "7",
+      "transactionMemberName": "Ivan Makmur",
+      "transactionMemberPhoneNumber": "06000000000",
+      "transactionMemberEmail": "ivan1@gmail.com",
+      "moneySent": "-81000",
+      "date": "2023-11-01"
+    },
+    {
+      "transactionId": "8",
+      "transactionMemberName": "Ivan Makmur",
+      "transactionMemberPhoneNumber": "06000000000",
+      "transactionMemberEmail": "ivan1@gmail.com",
+      "moneySent": "-81000",
+      "date": "2023-11-01"
+    },
+    {
+      "transactionId": "9",
+      "transactionMemberName": "Hans Santoso",
+      "transactionMemberPhoneNumber": "09000000000",
+      "transactionMemberEmail": "hans1@gmail.com",
+      "moneySent": "+1000",
+      "date": "2023-11-01"
+    },
+    {
+      "transactionId": "9",
+      "transactionMemberName": "Hans Santoso",
+      "transactionMemberPhoneNumber": "09000000000",
+      "transactionMemberEmail": "hans1@gmail.com",
+      "moneySent": "+1000",
+      "date": "2023-11-01"
+    },
+   
