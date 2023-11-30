0ba523334f1c6c013ed797c7917e9df25476d75e
rizkysugihartooo <145679788+rizkysugihartooo@users.noreply.github.com>
2023-12-01 01:15:48 +0700

    Update onboarding_screen.dart

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -85,3 +85,50 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   }
 
   List<Color> get colors => pages.map((p) => p.bgColor).toList();
+@override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: ConcentricPageView(
+          colors: colors,
+          radius: 30,
+          curve: Curves.ease,
+          duration: Duration(seconds: 2),
+          itemCount: pages.length,
+          itemBuilder: (index, value) {
+            // PageData page = pages[index % pages.length];
+            PageData page = pages[index];
+            return Column(children: [
+              Container(
+                child: Theme(
+                  data: ThemeData(
+                    textTheme: TextTheme(
+                      headline6: TextStyle(
+                          color: page.textColor,
+                          fontWeight: FontWeight.w600,
+                          fontFamily: 'Helvetica',
+                          letterSpacing: 0.0,
+                          fontSize: 15),
+                      subtitle2: TextStyle(
+                        color: page.textColor,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 18,
+                      ),
+                      bodyText2: GoogleFonts.poppins(
+                        color: page.textColor,
+                        letterSpacing: 0.0,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                  child: PageCard(page: page),
+                ),
+              ),
+            ]);
+          },
+        ),
+      ),
+    );
+  }
+}
