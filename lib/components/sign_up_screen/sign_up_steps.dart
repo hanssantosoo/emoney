86a4d54faefe3b5ce8e0de869f4abed2e7bfb1f1
lekreys <146582368+lekreys@users.noreply.github.com>
2023-12-01 01:22:47 +0700

    Update sign_up_steps.dart

--- lib/components/sign_up_screen/sign_up_steps.dart
+++ lib/components/sign_up_screen/sign_up_steps.dart
@@ -1,1 +1,66 @@
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:emoney/components/sign_up_screen/choose_username_screen.dart';
+import 'package:emoney/components/sign_up_screen/step_get_bank_account.dart';
+import 'package:emoney/components/sign_up_screen/step_get_email_password.dart';
+import 'package:emoney/components/sign_up_screen/step_get_name_address.dart';
+import 'package:emoney/utilities/slide_right_route.dart';
 
+class SignUpSteps extends StatefulWidget {
+  const SignUpSteps({Key? key}) : super(key: key);
+
+  @override
+  _SignUpStepsState createState() => _SignUpStepsState();
+}
+
+class _SignUpStepsState extends State<SignUpSteps> {
+  late PageController _signUpStepController;
+  final nameAddressFormKey = LabeledGlobalKey<FormState>("nameAddressForm");
+  final emailPasswordFormKey = LabeledGlobalKey<FormState>("emailPasswordForm");
+  final bankAccountFormKey = LabeledGlobalKey<FormState>("bankAccountForm");
+  Map<String, String> signUpDetails = {
+    'fullname': '',
+    'address': '',
+    'emailId': '',
+    'password': '',
+    'bankAccount': ''
+  };
+  Map<String, String> registraionDetails() => signUpDetails;
+  int _currentStep = 0;
+  List<bool> stepHasError = [false, false, false];
+  List<bool> stepCompletedSuccessfully = [false, false, false];
+  late List<Widget> signUpStepContent;
+  bool confirmSignUpButton = false;
+  @override
+  void initState() {
+    _signUpStepController = PageController();
+    signUpStepContent = [
+      StepGetNameAddress(
+        registrationDetails: registraionDetails,
+        updateSignUpDetails: updateSignUpDetails,
+        nameAddressFormKey: nameAddressFormKey,
+        proceedToNextStep: _proceedToNextStep,
+      ),
+      StepGetEmailPassword(
+        updateSignUpDetails: updateSignUpDetails,
+        emailPasswordFormKey: emailPasswordFormKey,
+        registrationDetails: registraionDetails,
+        proceedToNextStep: _proceedToNextStep,
+      ),
+      StepGetBankAccount(
+        updateSignUpDetails: updateSignUpDetails,
+        bankAccountFormKey: bankAccountFormKey,
+        registrationDetails: registraionDetails,
+        showConfirmSignUpButton: showConfirmSignUpButton,
+        finalStepProccessing: _finalStepProccessing,
+      )
+    ];
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _signUpStepController.dispose();
+    super.dispose();
+  }
