1e9aa9e1a37011b74fc768a0f292a0fd8952e0bc
Wansansss <113201428+Wansansss@users.noreply.github.com>
2023-12-01 13:21:26 +0700

    Update icons.dart

--- lib/utilities/icons.dart
+++ lib/utilities/icons.dart
@@ -5,3 +5,7 @@ class HadWinIcons {
 
   static const _kFontFam = 'HadWinIcons';
   static const String? _kFontPkg = null;
+  static const IconData wallet_business_and_trade = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData wallet_education_29 = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData line_awesome_wallet_solid = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
