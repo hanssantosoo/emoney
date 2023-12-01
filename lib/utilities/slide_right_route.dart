de8c0298a3493242f9ef77220334f1c2e4cc6ac7
Ipann23 <150114298+Ipann23@users.noreply.github.com>
2023-12-01 13:05:33 +0700

    Update slide_right_route.dart

--- lib/utilities/slide_right_route.dart
+++ lib/utilities/slide_right_route.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
 
+class SlideRightRoute extends PageRouteBuilder {
+  final Widget page;
+  SlideRightRoute({required this.page})
+      : super(
+          pageBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+          ) =>
+              page,
+          transitionsBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            Widget child,
+          ) =>
