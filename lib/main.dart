6e77a39ce12c772d5788144e7752f17c7e896928
hanssantosoo <150638666+hanssantosoo@users.noreply.github.com>
2023-12-01 17:06:53 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,238 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:emoney/screens/onboarding_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MyApp(),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'HelloPay',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF6200EE)),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'HelloPay.'),
-    );
-  }
-}
-
-class BalanceWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: const Color(0xFF6200EE),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Balance',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 12,
-            ),
-          ),
-          SizedBox(height: 4),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Icon(
-                Icons.account_balance_wallet,
-                color: Colors.white,
-                size: 24,
-              ),
-              SizedBox(width: 8),
-              Text(
-                '\Rp.100.000',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: 16),
-          // Icon buttons with text
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Column(
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      // Add your Top Up functionality here
-                    },
-                    icon: Column(
-                      children: [
-                        Icon(Icons.add, color: Colors.white, size: 24),
-                        Text('Top Up', style: TextStyle(color: Colors.white, fontSize: 12)),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      // Add your Transfer functionality here
-                    },
-                    icon: Column(
-                      children: [
-                        Icon(Icons.swap_horiz, color: Colors.white, size: 24),
-                        Text('Transfer', style: TextStyle(color: Colors.white, fontSize: 12)),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      // Add your History functionality here
-                    },
-                    icon: Column(
-                      children: [
-                        Icon(Icons.history, color: Colors.white, size: 24),
-                        Text('History', style: TextStyle(color: Colors.white, fontSize: 12)),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  int _selectedIndex = 0;
-
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text('Home'),
-    Text('Deposit'),
-    Text('Profile'),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Row(
-          children: [
-            Text(
-              widget.title,
-              style: TextStyle(
-                color: const Color(0xFF6200EE),
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Spacer(),
-            IconButton(
-              onPressed: () {
-                // Add your search functionality here
-              },
-              icon: Icon(Icons.search, color: const Color(0xFF6200EE)),
-            ),
-            IconButton(
-              onPressed: () {
-                // Add your message functionality here
-              },
-              icon: Icon(Icons.message, color: const Color(0xFF6200EE)),
-            ),
-            IconButton(
-              onPressed: () {
-                // Add your notification functionality here
-              },
-              icon: Icon(Icons.notifications, color: const Color(0xFF6200EE)),
-            ),
-          ],
-        ),
-      ),
-      body: Column(
-        children: [
-          // Balance Widget
-          BalanceWidget(),
-          // Main Content
-          Expanded(
-            child: Center(
-              child: _widgetOptions.elementAt(_selectedIndex),
-            ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.3),
-              spreadRadius: 2,
-              blurRadius: 5,
-              offset: Offset(0, 3),
-            ),
-          ],
-        ),
-        child: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.payment), // Payment icon
-              label: 'Pay', // Changed text to Pay
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Profile',
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          selectedItemColor: const Color(0xFF6200EE),
-          onTap: _onItemTapped,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Add',
-        child: const Icon(Icons.payment), // Payment icon
-      ),
-    );
+        title: 'Hello-Pay',
+        theme: ThemeData(
+            primarySwatch: Colors.deepPurple,
+            textTheme:
+                GoogleFonts.manropeTextTheme(Theme.of(context).textTheme)),
+        home: OnboardingScreen(),
+        debugShowCheckedModeBanner: false);
   }
 }
