ad3bf48c40b936a0cbe58788cf4b2439bd899e80
Sabiicuu <152463472+Sabiicuu@users.noreply.github.com>
2023-12-01 10:05:36 +0700

    Update local_splash_screen_component.dart

--- lib/components/main_app_screen/local_splash_screen_component.dart
+++ lib/components/main_app_screen/local_splash_screen_component.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LocalSplashScreenComponent extends StatelessWidget {
+  const LocalSplashScreenComponent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0x280494),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/system_image/E-WalletP.png',
+            height: 128.0,
+          ),
+        ],
+      ),
+    );
+  }
+}
 
