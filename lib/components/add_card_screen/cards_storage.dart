6479568e4a6150a3955b967620e1d247a76ca9fa
Ipann23 <150114298+Ipann23@users.noreply.github.com>
2023-12-01 15:39:14 +0700

    Update cards_storage.dart

--- lib/components/add_card_screen/cards_storage.dart
+++ lib/components/add_card_screen/cards_storage.dart
@@ -10,3 +10,14 @@ class CardsStorage {
 
     return directory.path;
   }
+
+  Future<File> get _cardsFile async {
+    final path = await _localPath;
+    return File('$path/hadwin_available_cards.json');
+  }
+
+  Future<Map<String, dynamic>> get randomCard async {
+    var _availableCards = (await readAvailableCards())['availableCards'];
+
+    return _availableCards[Random().nextInt(_availableCards.length)];
+  }
