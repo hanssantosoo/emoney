7b98ebe2b66740dcecb830d963a86800535fb8b5
Ipann23 <150114298+Ipann23@users.noreply.github.com>
2023-12-01 15:39:02 +0700

    Update cards_storage.dart

--- lib/components/add_card_screen/cards_storage.dart
+++ lib/components/add_card_screen/cards_storage.dart
@@ -1,1 +1,12 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
 
+import 'package:path_provider/path_provider.dart';
+
+class CardsStorage {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
