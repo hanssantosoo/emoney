bf55e69d4131004375684d413ec29172cdd3d6ac
rizkysugihartooo <145679788+rizkysugihartooo@users.noreply.github.com>
2023-12-01 15:49:03 +0700

    Update card_processing_screen.dart

--- lib/components/add_card_screen/card_processing_screen.dart
+++ lib/components/add_card_screen/card_processing_screen.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 
+class CardProcessingScreen extends StatefulWidget {
+  const CardProcessingScreen({Key? key, required this.cardDetails})
+      : super(key: key);
+  final Map<String, String> cardDetails;
+  @override
+  State<CardProcessingScreen> createState() => _CardProcessingScreenState();
+}
