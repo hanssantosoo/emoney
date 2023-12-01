f8dc8637abda45bdc302ee7e4013fed7a813290b
rizkysugihartooo <145679788+rizkysugihartooo@users.noreply.github.com>
2023-12-01 13:13:02 +0700

    Update custom_date_grouping.dart

--- lib/utilities/custom_date_grouping.dart
+++ lib/utilities/custom_date_grouping.dart
@@ -1,1 +1,21 @@
 
+
+int calcDaysBetween(DateTime from, DateTime to) {
+  from = DateTime(from.year, from.month, from.day);
+  to = DateTime(to.year, to.month, to.day);
+  return (to.difference(from).inHours ~/ 24);
+}
+
+int calcSecondsBetween(DateTime from, DateTime to) {
+  from = DateTime(
+      from.year, from.month, from.day, from.hour, from.minute, from.second);
+  to = DateTime(to.year, to.month, to.day, to.hour, to.minute, to.second);
+  return to.difference(from).inSeconds;
+}
+
+String customGroup(DateTime transactionDate) {
+  String response = '';
+  DateTime latestDate = DateTime.now();
+  int nDaysInBetween = calcDaysBetween(transactionDate, latestDate);
+ 
+  int years = nDaysInBetween ~/ 365;
