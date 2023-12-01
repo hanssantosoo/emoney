ec7f57fc7356e5c5c2ec2293399e5efc30591961
lekreys <146582368+lekreys@users.noreply.github.com>
2023-12-01 10:03:36 +0700

    Update loading_screen_component.dart

--- lib/components/main_app_screen/loading_screen_component.dart
+++ lib/components/main_app_screen/loading_screen_component.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreenComponent extends StatelessWidget {
+  const LoadingScreenComponent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 64,
+            width: 64,
+            child: CircularProgressIndicator(),
+          ),
+          Padding(padding: EdgeInsets.all(10), child: Text("Loading..."))
+        ],
+      ),
+    );
+  }
+}
 
