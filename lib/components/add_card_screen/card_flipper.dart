77b82b7bc3ba92fc07d747fdd84e09c0d923fb81
Wansansss <113201428+Wansansss@users.noreply.github.com>
2023-12-01 15:43:58 +0700

    Update card_flipper.dart

--- lib/components/add_card_screen/card_flipper.dart
+++ lib/components/add_card_screen/card_flipper.dart
@@ -1,1 +1,19 @@
+import 'dart:math';
 
+import 'package:flutter/material.dart';
+
+class CardFlipper extends StatefulWidget {
+  final Widget frontSide, backSide;
+  final Duration transitionDuration;
+  final CardFlippingController? cardFlippingController;
+  CardFlipper(
+      {Key? key,
+      required this.frontSide,
+      required this.backSide,
+      required this.transitionDuration,
+      this.cardFlippingController})
+      : super(key: key);
+
+  @override
+  _CardFlipperState createState() => _CardFlipperState();
+}
