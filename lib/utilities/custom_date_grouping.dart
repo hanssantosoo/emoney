696890786b81e0fb595f60bd229e0c0b94b75bc7
rizkysugihartooo <145679788+rizkysugihartooo@users.noreply.github.com>
2023-12-01 13:16:22 +0700

    Update custom_date_grouping.dart

--- lib/utilities/custom_date_grouping.dart
+++ lib/utilities/custom_date_grouping.dart
@@ -210,3 +210,25 @@ String _formatDay(int day) {
   }
   return "$day$suffix";
 }
+//? FUNCTION FOR RETRIEVING ORDINALS
+String _formatDay(int day) {
+  String suffix = '';
+  if (day >= 11 && day<=13) {
+    suffix = 'th';
+  } else {
+    switch (day % 10) {
+      case 1:
+        suffix = 'st';
+        break;
+      case 2:
+        suffix = 'nd';
+        break;
+      case 3:
+        suffix = 'rd';
+        break;
+      default:
+        suffix = 'th';
+    }
+  }
+  return "$day$suffix";
+}
