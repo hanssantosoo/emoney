22a2e3bc77ead04d66998e73617485dd8f1abe3f
rizkysugihartooo <145679788+rizkysugihartooo@users.noreply.github.com>
2023-12-01 13:18:35 +0700

    Update display_error_alert.dart

--- lib/utilities/display_error_alert.dart
+++ lib/utilities/display_error_alert.dart
@@ -1,1 +1,10 @@
-
+import 'package:flutter/material.dart';
+void showErrorAlert(BuildContext context, Map<String, dynamic> error) {
+  Map<String, dynamic> errorTypes = {
+    'error': _CommonError(context, error),
+    'internetConnectionError': _LocalError(context, error),
+    'localDBError': _LocalError(context, error),
+    'authenticationError': _CommonError(context, error),
+    'apiAuthorizationError': _HazardousError(context, error),
+    'corruptedTokenError': _HazardousError(context, error)
+  };
