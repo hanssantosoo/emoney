6c453c049790544102f766fd5443a0a4acfcb4fa
rizkysugihartooo <145679788+rizkysugihartooo@users.noreply.github.com>
2023-12-01 13:18:55 +0700

    Update display_error_alert.dart

--- lib/utilities/display_error_alert.dart
+++ lib/utilities/display_error_alert.dart
@@ -8,3 +8,50 @@ void showErrorAlert(BuildContext context, Map<String, dynamic> error) {
     'apiAuthorizationError': _HazardousError(context, error),
     'corruptedTokenError': _HazardousError(context, error)
   };
+  String currentError = error.keys.first;
+
+  showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+            title: Text(
+              "Error",
+              textAlign: TextAlign.center,
+            ),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: errorTypes[currentError].errorDescription,
+            ),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            actionsAlignment: MainAxisAlignment.center,
+            actions: [
+              Container(
+                height: 48,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.blueGrey.shade100,
+                        offset: Offset(0, 4),
+                        blurRadius: 5.0)
+                  ],
+                  gradient: RadialGradient(
+                      colors: [Color(0xff0070BA), Color(0xff1546A0)],
+                      radius: 8.4,
+                      center: Alignment(-0.24, -0.36)),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: ElevatedButton(
+                    onPressed: errorTypes[currentError].onClose,
+                    child: Text('OK'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.transparent,
+                      shadowColor: Colors.transparent,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    )),
+              )
+            ],
+          ));
+}
